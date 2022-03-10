167665fae5ce3ed0b8b60fe99b614bcd54711f3d
Akim Mukhtarov <akimismyname@gmail.com>
2022-03-10 20:43:26 +0300

    a wrapper to properly represent workplace and an up to date state of its related resources

---
+++ wms_boost/app/workplace/workplace.py
@@ -0,0 +1,20 @@
+from typing import Any, Dict
+from app.models import Workplace as DbWorkplace
+from app.refuns_dump import TodayDump
+from werkzeug.utils import cached_property
+
+
+class Workplace:
+    """ Wrap workplace representation in db,
+        and fetch related resource from Redis using cache """
+    def __init__(self, workplace_in_db: DbWorkplace):
+        self._db_state = workplace_in_db
+
+    @cached_property
+    def refunds_dump(self) -> TodayDump:
+        # return value from redis, use cache
+        return TodayDump(self._db_state)
+
+    def __getattr__(self, attr) -> Any:
+        # proxy to underlying db state
+        return getattr(self._db_state, attr)
