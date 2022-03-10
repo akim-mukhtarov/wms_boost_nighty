bdc32c9566f8f462549fbace400045b75885feeb
Akim Mukhtarov <akimismyname@gmail.com>
2022-03-11 01:51:18 +0300

    ...

--- wms_boost/run.py
+++ wms_boost/run.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 if __name__ == '__main__':
-    uvicorn.run('app:app', port=5000, debug=True, access_log=False)
+    uvicorn.run('app:app', port=5054, debug=True, access_log=False)
