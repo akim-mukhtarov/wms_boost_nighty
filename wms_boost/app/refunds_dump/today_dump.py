0b0a2851da24fcc2eb4aac88141d956c14051f3a
Akim Mukhtarov <akimismyname@gmail.com>
2022-03-10 21:23:12 +0300

    Wrap const params from DB and mutable values from Redis

--- wms_boost/app/refunds_dump/today_dump.py
+++ wms_boost/app/refunds_dump/today_dump.py
@@ -6,30 +6,36 @@ import redis
 
 
 # helper
-def _get_next_midnight(client_time: ClientTime) -> int:
+def _get_next_midnight(timezone_name: str) -> int:
+    client_time = ClientTime(timezone_name)
     pass
 
 
 class TodayDump:
-    """ Wrap LastRefundsDump from db in order to provide
-        access to its up-to-date values (both from db and Redis) """
+    """ Wrap const params from DB and mutable values from Redis """
+
+    _default_state = {
+        'included': 0,
+        'processed': 0,
+        'status': 'default'
+    }
+
     def __init__(self, workplace: Workplace):
-        self._client_time = ClientTime(workplace)
         self._redis_conn = redis.Redis(connection_pool=REDIS_POOL)
 
         last_dump = workplace.last_refunds_dump
 
-        self._workplace_id = last_dump.workplace_id
         self._id = last_dump.id
+        self._workplace_id = last_dump.workplace_id
         self._report_url = last_dump.report_url
         self._redis_key = last_dump.redis_key
-        # just placeholders for redis values
-        self._included = None
-        self._processed = None
-        self._status = None
+        self._timezone = workplace.timezone
 
-        self._init_from_redis(self, self._redis_key)
-
+        state = self._get_state(self._redis_conn, self._redis_key)
+        self._included = state['included']
+        self._processed = state['processed']
+        self._status = state['status']
+        self._state_dict = state
 
     @property
     def id(self) -> int:
@@ -55,13 +61,11 @@ class TodayDump:
     def status(self) -> str:
         return self._status
 
-
     def add_new(self, just_included: int) -> None:
         # supposed to be used by `dump_refunds` functio
         # update `included` field and write out changes to Redis
         self._included += just_included
-        self._update_redis_values(('included', self._included))
-
+        self._update_state(('included', self._included))
 
     def update_progress(self, processed: int) -> None:
         # update `processed` field and write out changes to Redis
@@ -69,36 +73,25 @@ class TodayDump:
         values = [('processed', processed)]
         if self._processed == self.included:
             values.append(('status', 'completed'))
-        self._update_redis_values(values)
+        self._update_state(values)
 
+    def _update_state(*args) -> None:
+        """ Update/set values stored in Redis
+            setup expiration date to the end of the current day """
+        for k, v in args:
+            self._state_dict[k] = v
 
-    def _update_redis_values(*args) -> None:
-        new_state = {
-            k : v
-                for k, v in args
-        }
         r = self._redis_conn
-        r.hmset(self._redis_key, new_state)
+        r.hmset(self._redis_key, self._state_dict)
         r.expireat(
                 self._redis_key,
-                _get_next_midnight())
+                _get_next_midnight(self._timezone))
 
-
-    def _init_from_redis(self, redis_key: str) -> None:
-        r = self._redis_conn
-        # check existance/expiration
+    @classmethod
+    def _get_state(cls, r: redis.Redis, redis_key: str) -> Dict:
+        """ Get values from Redis, if key exists.
+            Return default otherwise """
         if not r.exists(redis_key) or r.pttl(redis_key) <= 0:
-            # set default values if key has expired or not exists
-            included = 0
-            processed = 0
-            status = "default"
+            return cls._default_state
         else:
-            # use redis values otherwise
-            state_dict = r.hgetall(redis_key)
-            included = state_dict['included']
-            processed = state_dict['processed']
-            status = state_dict['status']
-
-        self._included = included
-        self._processed = processed
-        self._status = status
+            return r.hgetall(redis_key)
