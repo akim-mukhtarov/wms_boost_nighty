2016235587c77088d4ee68f7fdb2485c9218d4a8
Akim Mukhtarov <akimismyname@gmail.com>
2022-03-10 20:43:48 +0300

    edit crud ops

--- wms_boost/app/workplace/crud.py
+++ wms_boost/app/workplace/crud.py
@@ -1,5 +1,11 @@
-from app.models import Workplace, LastRefundsDump, StorageStepsProgress
+from app.models import (
+    Workplace,
+    RefundsDumpSettings,
+    StorageStepsSettings
+)
+from sqlalchemy.orm import Session
 
+from .errors import WorkplaceNotFound
 from .schemas import WorkplaceCreate
 
 
@@ -10,12 +16,21 @@ def init_workplace(data: WorkplaceCreate):
         short_name = data.short_name,
         timezone = data.timezone)
 
-    refunds_dump = LastRefundsDump(
+    refunds_dump_settings = RefundsDumpSettings(
         workplace_id = data.wms_key,
-        report_url = data.reports_urls.refunds_dump)
+        report_url = data.reports_urls.refunds_dump,
+        redis_key = RefundsDumpSettings.generate_redis_key(workplace_id))
 
-    storage_steps = StorageStepsProgress(
+    storage_steps_settings = StorageStepsSettings(
         workplace_id = data.wms_key,
-        report_url = data.reports_urls.storage_steps)
+        report_url = data.reports_urls.storage_steps,
+        redis_key = StorageStepsSettings.generate_redis_key(workplace_id))
 
-    return workplace, refunds_dump, storage_steps
+    return workplace, refunds_dump_settings, storage_steps_settings
+
+
+def get_workplace(db: Session, workplace_id: int) -> Workplace:
+    workplace = db.query(Workplace).get(workplace_id)
+    if not workplace:
+        raise WorkplaceNotFound()
+    return workplace
