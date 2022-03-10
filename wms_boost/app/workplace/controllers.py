59a5088fac3cb39440c17564de5aae732971ddec
Akim Mukhtarov <akimismyname@gmail.com>
2022-03-10 20:45:26 +0300

    edit controllers

--- wms_boost/app/workplace/controllers.py
+++ wms_boost/app/workplace/controllers.py
@@ -4,7 +4,8 @@ from app import app
 from app.db import get_db
 from app.auth import get_current_user
 from app.auth.security import raw_decode
-from app.models import User, Workplace
+from app.models import User
+from app.models import Workplace as DbWorkplace
 from app.rpc import rpc_method
 
 from fastapi import Depends, WebSocket, status
@@ -13,10 +14,11 @@ from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 from jsonrpcserver import Result, Success, Error
 
-from .errors import WorkplaceNotFound
 from .crud import init_workplace as init_db_workplace
+from .crud import get_workplace
 from .workplace_observer import WorkplaceObserver
 from . import schemas
+from .workplace import Workplace
 
 
 @rpc_method
@@ -25,8 +27,8 @@ def init_workplace(data: schemas.WorkplaceCreate) -> Result:
             `LastRefundsDump` and `StorageStepsProgress` """
     db = next(iter(get_db()))
 
-    workplace = db.query(Workplace).get(data.wms_key)
-    if workplace:
+    db_workplace = db.query(DbWorkplace).get(data.wms_key)
+    if db_workplace:
         return Error(6, "Workplace with provided wms_key already exists")
 
     db.add_all(init_db_workplace(data))
@@ -34,16 +36,14 @@ def init_workplace(data: schemas.WorkplaceCreate) -> Result:
     return Success()
 
 
-@app.get('/workplace/{workplace_id}', response_model=schemas.Workplace)
+@app.get('/workplace/{workplace_id}', response_model=schemas.WorkplaceModel)
 def get_workplace(
         workplace_id: int,
         db: Session = Depends(get_db),
         user: User = Depends(get_current_user)
 ):
-    workplace = db.query(Workplace).get(workplace_id)
-    if not workplace:
-        raise WorkplaceNotFound()
-    return workplace
+    db_workplace = get_workplace(db, workplace_id)
+    return Workplace(db_workplace)
 
 # helper
 async def auth_connection(ws: WebSocket) -> bool:
@@ -65,5 +65,5 @@ async def workplace_updates(ws: WebSocket, workplace_id: int):
             .on_update(ws.send_json)\
             .check_alive(lambda: ws.client_state.name == "CONNECTED")
 
-    await observer.poll(period=5)
+    await observer.observe()
     await ws.close()    # ??
