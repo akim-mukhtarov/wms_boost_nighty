26ee739359c8b97b22087dc005f2e2e921752f4d
Akim Mukhtarov <akimismyname@gmail.com>
2022-03-11 15:22:47 +0300

    implement context management for connection

--- wms_boost/app/sheets_service/sheets_service.py
+++ wms_boost/app/sheets_service/sheets_service.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 
 
 class SheetsService:
@@ -7,10 +8,23 @@ class SheetsService:
         def send(self, method_name: str, args: Tuple) -> None:
             pass
 
+        def close(self) -> None:
+            pass
+
     def append(self, sheet_url: str, data) -> int:
         with self._connect() as conn:
             conn.send("append", (sheet_url, data))
         return len(data)
 
-    def _connect(self) -> SheetsService.Connection:
-        pass
+    @contextmanager
+    def _connect(self):
+        try:
+            conn = SheetsService.Connection()
+            yield conn
+            
+        except Exception:
+            conn.close()
+            raise e
+
+        finally:
+            conn.close()
