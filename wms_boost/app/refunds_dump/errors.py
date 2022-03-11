654196f0fbdbe88553de93185f17b5f4da3d30fa
Akim Mukhtarov <akimismyname@gmail.com>
2022-03-11 16:21:29 +0300

    add stumb for `AlreadyProcessed`

--- wms_boost/app/refunds_dump/errors.py
+++ wms_boost/app/refunds_dump/errors.py
@@ -1,11 +1,4 @@
 from jsonrpcserver import Error
-
-
-class AlreadyProcessed(Exception):
-    _msg = "Refunds dump is already being processed"
-
-    def __init__(self):
-        super().__init__(self._msg)
 # TODO: consider handling wms-related excpetions in separate utility?
 # to aboid copying the same code
 def wrap_exception(exc: Exception):
@@ -17,3 +10,5 @@ def wrap_exception(exc: Exception):
         return Error()  # 501
     elif isinstance(WmsServerError):
         return Error()  # 503
+    elif isinstance(AlreadyProcessed):
+        return Error() # precondition is false
