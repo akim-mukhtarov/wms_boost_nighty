8f1773fb62305dc6a7a7e9ead7525988ab485b34
Akim Mukhtarov <akimismyname@gmail.com>
2022-03-12 23:07:29 +0300

    minor changes

--- wms_boost/app/utils/client_time.py
+++ wms_boost/app/utils/client_time.py
@@ -8,6 +8,7 @@ class ClientTime:
         self._timezone = pytz.timezone(timezone)
         self._today_date = None
 
+    # TODO: rewrite with werkzeug.cached_property
     def get_today_date(self) -> datetime.date:
         if self._today_date:
             return self._today_date
@@ -27,14 +28,13 @@ class ClientTime:
         today_date = self.get_today_date()
         date_with_offset = today_date + datetime.timedelta(days=days)
         # NOTE: return datetime instead of a date
-        return datetime.datetime(
-            year=n_days_ago.year,
-            month=n_days_ago.month,
-            day=n_days_ago.day,
+        dt = datetime.datetime(
+            year = date_with_offset.year,
+            month = date_with_offset.month,
+            day = date_with_offset.day,
         )
         # reinterpret in client's timezone !
-        dt = self._timezone.localize(dt)
-        return dt
+        return self._timezone.localize(dt)
 
     def get_days_ago(self, days) -> datetime.datetime:
         n_days_ago = self.get_today_date() - datetime.timedelta(days)
