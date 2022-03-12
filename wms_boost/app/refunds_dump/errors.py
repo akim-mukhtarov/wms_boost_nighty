cab5e5346d02677077dbff009b23f991899c55c7
Akim Mukhtarov <akimismyname@gmail.com>
2022-03-12 20:42:56 +0300

    fix imports

--- wms_boost/app/refunds_dump/errors.py
+++ wms_boost/app/refunds_dump/errors.py
@@ -1,6 +1,6 @@
 from jsonrpcserver import Error
 from app.rpc import RpcErrors
-from app.wms_api import AuthenticationError
+from app.wms_api import AuthenticationError, UnexpectedApiResponse
 from .refunds_dumper import AlreadyProcessed
 # TODO: consider handling wms-related excpetions in separate utility?
 # to aboid copying the same code
