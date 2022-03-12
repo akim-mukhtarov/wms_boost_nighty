90d750992c030e32350f8be529fb54ab07e1e8f7
Akim Mukhtarov <akimismyname@gmail.com>
2022-03-12 20:51:55 +0300

    add error messages

--- wms_boost/app/refunds_dump/errors.py
+++ wms_boost/app/refunds_dump/errors.py
@@ -9,20 +9,30 @@ from app.wms_api import (
 from .refunds_dumper import AlreadyProcessed
 # TODO: consider handling wms-related excpetions in separate utility?
 # to aboid copying the same code
+
+# TODO: finally add some RpcError constructor to not shitpost this everywhere!
 def wrap_exception(exc: Exception):
     """ An utility to wrap exceptions while
         dump refunds in JSONRPC error format """
     if isinstance(exc, AuthenticationError):
-        return Error(RpcErrors.NOT_AUTHORIZED)
+        return Error(
+                RpcErrors.NOT_AUTHORIZED,
+                "Not authorized")
 
     elif isinstance(exc, UnexpectedApiResponse):
-        return Error(RpcErrors.UNIMPLEMENTED)
+        return Error(
+                RpcErrors.UNIMPLEMENTED,
+                "Side API returned unsupported response")
 
     elif isinstance(exc, WmsServerError):
-        return Error(RpcErrors.UNAVAILABLE)
+        return Error(
+                RpcErrors.UNAVAILABLE,
+                "Side API is unavailable")
 
     elif isinstance(exc, AlreadyProcessed):
-        return Error(RpcErrors.FAILED_PRECONDITION)
+        return Error(
+                RpcErrors.FAILED_PRECONDITION,
+                "Precondition failed")
 
     else:
-        return Error(RpcErrors.INTERNAL)
+        return Error(RpcErrors.INTERNAL, "Internal error")
